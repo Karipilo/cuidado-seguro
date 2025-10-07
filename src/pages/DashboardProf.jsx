53c0fc93d452910c97b1e8481cd9073029364bfc
Carlos Bernal <car.bernal@duocuc.cl>
2025-10-07 11:23:45 -0300

    Dejando los campos por defecto

---
+++ src/pages/DashboardProf.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DashboardProf() {
+  return (
+    <div>
+      <h1>Dashboard Prof</h1>
+      <p>Welcome to the professor's dashboard!</p>
+    </div>
+  );
+}
+
+export default DashboardProf;
--- src/pages/DashboardTutor.jsx
+++ src/pages/DashboardTutor.jsx
@@ -343,3 +343,4 @@ const DashboardTutor = () => {
 };
 
 export default DashboardTutor;
+
