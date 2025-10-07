dffd43e4f736760251e63ec9c4998bf447162db7
KarinaPimentel <ka.pimentel@duocuc.cl>
2025-10-07 10:37:47 -0300

    test

--- src/testing/Footer.test.jsx
+++ src/testing/Footer.test.jsx
@@ -1,5 +1,6 @@
+//Prueba unitaria del Footer
 //importar el componente a testear
-import { describe, it } from "vitest";
+import { describe, it, vi } from "vitest";
 import Footer from "../components/Footer";
 //obtener las herramientas necesarias. Firevent es para simular eventos como click, submit, etc.
 import { render, screen, fireEvent } from "@testing-library/react";
@@ -9,10 +10,17 @@ describe("Comportamiento del Footer", () => {
     it("Debería renderizarse correctamente", () => {
         render(<Footer />);
         //esperar que desde la pantalla (screen) se vea el texto "© 2024 Cuidado Seguro - Todos los derechos reservados."
-        expect(screen.getByAltText("© 2024 Cuidado Seguro - Todos los derechos reservados.")).roBeInTheDocument()
+        expect(screen.getAllByText("© 2024 Cuidado Seguro - Todos los derechos reservados.")).toBeInTheDocument()
     })
     it("Ejecuta Alerta", () => {
         //un espia lo más simple posible
-        const alert = vi.fn()
+        const alert = vi.spyOn(window, "alert").mockImplementation(() => { })
+        render(<Footer />)
+
+        fireEvent.click(screen.getByText("© 2024 Cuidado Seguro - Todos los derechos reservados."));
+
+
+        //espero que mi espia se llame una vez
+        expect(alert).toHaveBeenCalledTimes(1)
     })
 })
