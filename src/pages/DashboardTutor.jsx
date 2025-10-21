5f3af4177a9fad86678a0a734d04d86c54c8ebb4
Carlos271085 <car.bernal@duocuc.cl>
2025-10-21 10:23:57 -0300

    Primer commit del Proyecto_Cuidado_seguro

--- src/pages/DashboardTutor.jsx
+++ src/pages/DashboardTutor.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+
+
+
 
 function DashboardTutor() {
 	/*return (
